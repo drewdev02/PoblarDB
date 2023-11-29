1d2927402439ce1470f372f6febddf34504afbcf
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-11-28 19:16:23 -0500

    mod

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,5 @@ G
 src/main/java/Main.java,e\4\e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
 K
 src/main/java/DataBase.java,8\a\8a3345b1c694c0313db79fd4936e82440f16761e
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,24 @@
+import com.thedeanda.lorem.Lorem;
+import com.thedeanda.lorem.LoremIpsum;
+
 import java.sql.SQLException;
 import java.util.Random;
 
 public class Main {
     public static Random random = new Random();
+    public static Lorem lorem = LoremIpsum.getInstance();
 
     public static void main(String[] args) {
         try {
             DataBase.getInstancia();
             for (int i = 0; i < 10; i++) {
+
+                var password = "123456";
+                var name = lorem.getName();
+                DataBase.insertUser(password, name, "email" + i);
                 var role = "user";
                 DataBase.setRoles(role);
-                DataBase.insertUser()
+
             }
 
         } catch (SQLException e) {
